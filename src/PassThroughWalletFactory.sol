132a302fbc0c252b151ba57eb8955b945af4a69e
Will Minshew <wminshew@gmail.com>
2023-04-03 18:50:28 -0700

    init

---
+++ src/PassThroughWalletFactory.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import {LibClone} from "splits-utils/LibClone.sol";
+
+import {PassThroughWalletImpl} from "src/PassThroughWalletImpl.sol";
+
+/// @title Pass-Through Wallet Factory
+/// @author 0xSplits
+/// @notice Factory for creating pass-through wallets
+/// @dev This contract uses token = address(0) to refer to ETH.
+contract PassThroughWalletFactory {
+    /// -----------------------------------------------------------------------
+    /// libraries
+    /// -----------------------------------------------------------------------
+
+    using LibClone for address;
+
+    /// -----------------------------------------------------------------------
+    /// events
+    /// -----------------------------------------------------------------------
+
+    event CreatePassThroughWallet(
+        PassThroughWalletImpl indexed passThroughWallet, PassThroughWalletImpl.InitParams params
+    );
+
+    /// -----------------------------------------------------------------------
+    /// storage
+    /// -----------------------------------------------------------------------
+
+    /// -----------------------------------------------------------------------
+    /// storage - constants & immutables
+    /// -----------------------------------------------------------------------
+
+    PassThroughWalletImpl public immutable passThroughWalletImpl;
+
+    /// -----------------------------------------------------------------------
+    /// constructor
+    /// -----------------------------------------------------------------------
+
+    constructor() {
+        passThroughWalletImpl = new PassThroughWalletImpl();
+    }
+
+    /// -----------------------------------------------------------------------
+    /// functions
+    /// -----------------------------------------------------------------------
+
+    /// -----------------------------------------------------------------------
+    /// functions - public & external
+    /// -----------------------------------------------------------------------
+
+    function createPassThroughWallet(PassThroughWalletImpl.InitParams calldata params_)
+        external
+        returns (PassThroughWalletImpl passThroughWallet)
+    {
+        passThroughWallet = PassThroughWalletImpl(payable(address(passThroughWalletImpl).clone()));
+        passThroughWallet.initializer(params_);
+        emit CreatePassThroughWallet({passThroughWallet: passThroughWallet, params: params_});
+    }
+}
---
+++ src/PassThroughWalletImpl.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import {PausableImpl} from "splits-utils/PausableImpl.sol";
+import {TokenUtils} from "splits-utils/TokenUtils.sol";
+import {WalletImpl} from "splits-utils/WalletImpl.sol";
+
+/// @title Pass-Through Wallet Implementation
+/// @author 0xSplits
+/// @notice A clone-implementation of a pass-through wallet.
+/// Please be aware, owner has _FULL CONTROL_ of the deployment.
+/// @dev This contract uses token = address(0) to refer to ETH.
+contract PassThroughWalletImpl is WalletImpl, PausableImpl {
+    /// -----------------------------------------------------------------------
+    /// libraries
+    /// -----------------------------------------------------------------------
+
+    using TokenUtils for address;
+
+    /// -----------------------------------------------------------------------
+    /// structs
+    /// -----------------------------------------------------------------------
+
+    struct InitParams {
+        address owner;
+        bool paused;
+        address passThrough;
+    }
+
+    /// -----------------------------------------------------------------------
+    /// events
+    /// -----------------------------------------------------------------------
+
+    // emitted in clone bytecode
+    event ReceiveETH(uint256 amount);
+
+    /// -----------------------------------------------------------------------
+    /// storage
+    /// -----------------------------------------------------------------------
+
+    /// -----------------------------------------------------------------------
+    /// storage - constants & immutables
+    /// -----------------------------------------------------------------------
+
+    address public immutable passThroughWalletFactory;
+
+    /// -----------------------------------------------------------------------
+    /// storage - mutables
+    /// -----------------------------------------------------------------------
+
+    address public $passThrough;
+
+    /// -----------------------------------------------------------------------
+    /// constructor & initializer
+    /// -----------------------------------------------------------------------
+
+    constructor() {
+        passThroughWalletFactory = msg.sender;
+    }
+
+    function initializer(InitParams calldata params_) external {
+        // only passThroughWalletFactory may call `initializer`
+        if (msg.sender != passThroughWalletFactory) revert Unauthorized();
+
+        // don't need to init wallet separately
+        __initPausable({owner_: params_.owner, paused_: params_.paused});
+        $passThrough = params_.passThrough;
+    }
+
+    /// -----------------------------------------------------------------------
+    /// functions
+    /// -----------------------------------------------------------------------
+
+    /// -----------------------------------------------------------------------
+    /// functions - public & external
+    /// -----------------------------------------------------------------------
+
+    /// emit event when receiving ETH
+    /// @dev implemented w/i clone bytecode
+    /* receive() external payable { */
+    /*     emit ReceiveETH(msg.value); */
+    /* } */
+
+    /// pass tokens_ through to $passThrough
+    function passThroughTokens(address[] calldata tokens_) external pausable {
+        address passThrough = $passThrough;
+        uint256 length = tokens_.length;
+        for (uint256 i; i < length;) {
+            address token = tokens_[i];
+            token._safeTransfer(passThrough, token._balanceOf(address(this)));
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+}
