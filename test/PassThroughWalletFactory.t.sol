7e6a87604d8dcfbc7d69cb9d8537173707043a9a
Will Minshew <wminshew@gmail.com>
2023-04-17 14:33:15 -0700

    test clone

--- test/PassThroughWalletFactory.t.sol
+++ test/PassThroughWalletFactory.t.sol
@@ -3,33 +3,40 @@ pragma solidity ^0.8.17;
 
 import "splits-tests/base.t.sol";
 
-import {PassThroughWalletImpl, PassThroughWalletFactory} from "../src/PassThroughWalletFactory.sol";
+import {LibCloneBase} from "splits-tests/LibClone.t.sol";
 
-contract PassThroughWalletFactoryTest is BaseTest {
+import {PassThroughWalletFactory} from "../src/PassThroughWalletFactory.sol";
+import {PassThroughWalletImpl} from "../src/PassThroughWalletImpl.sol";
+
+contract PassThroughWalletFactoryTest is BaseTest, LibCloneBase {
     event CreatePassThroughWallet(
         PassThroughWalletImpl indexed passThroughWallet, PassThroughWalletImpl.InitParams params
     );
 
     PassThroughWalletFactory passThroughWalletFactory;
     PassThroughWalletImpl passThroughWalletImpl;
+    PassThroughWalletImpl passThroughWallet;
 
     PassThroughWalletImpl.InitParams params;
 
-    function setUp() public virtual override {
-        super.setUp();
+    function setUp() public virtual override(BaseTest, LibCloneBase) {
+        BaseTest.setUp();
 
         passThroughWalletFactory = new PassThroughWalletFactory();
         passThroughWalletImpl = passThroughWalletFactory.passThroughWalletImpl();
 
         params = PassThroughWalletImpl.InitParams({owner: users.alice, paused: false, passThrough: users.bob});
+        passThroughWallet = passThroughWalletFactory.createPassThroughWallet(params);
+
+        // setup LibCloneBase
+        impl = address(passThroughWalletImpl);
+        clone = address(passThroughWallet);
+        amount = 1 ether;
+        data = "Hello, World!";
     }
 
     /// -----------------------------------------------------------------------
-    /// tests - basic
-    /// -----------------------------------------------------------------------
-
-    /// -----------------------------------------------------------------------
-    /// tests - basic - createPassThroughWallet
+    /// createPassThroughWallet
     /// -----------------------------------------------------------------------
 
     function test_createPassThroughWallet_callsInitializer() public {
@@ -41,21 +48,6 @@ contract PassThroughWalletFactoryTest is BaseTest {
         passThroughWalletFactory.createPassThroughWallet(params);
     }
 
-    function test_createPassThroughWallet_emitsCreatePassThroughWallet() public {
-        address expectedAddress = _predictNextAddressFrom(address(passThroughWalletFactory));
-        _expectEmit();
-        emit CreatePassThroughWallet(PassThroughWalletImpl(expectedAddress), params);
-        passThroughWalletFactory.createPassThroughWallet(params);
-    }
-
-    /// -----------------------------------------------------------------------
-    /// tests - fuzz
-    /// -----------------------------------------------------------------------
-
-    /// -----------------------------------------------------------------------
-    /// tests - fuzz - createPassThroughWallet
-    /// -----------------------------------------------------------------------
-
     function testFuzz_createPassThroughWallet_callsInitializer(PassThroughWalletImpl.InitParams calldata params_)
         public
     {
@@ -63,8 +55,15 @@ contract PassThroughWalletFactoryTest is BaseTest {
             callee: address(passThroughWalletImpl),
             msgValue: 0 ether,
             data: abi.encodeCall(PassThroughWalletImpl.initializer, (params_))
-        });
+            });
         passThroughWalletFactory.createPassThroughWallet(params_);
+    }
+
+    function test_createPassThroughWallet_emitsCreatePassThroughWallet() public {
+        address expectedAddress = _predictNextAddressFrom(address(passThroughWalletFactory));
+        _expectEmit();
+        emit CreatePassThroughWallet(PassThroughWalletImpl(expectedAddress), params);
+        passThroughWalletFactory.createPassThroughWallet(params);
     }
 
     function testFuzz_createPassThroughWallet_emitsCreatePassThroughWallet(
@@ -75,4 +74,33 @@ contract PassThroughWalletFactoryTest is BaseTest {
         emit CreatePassThroughWallet(PassThroughWalletImpl(expectedAddress), params_);
         passThroughWalletFactory.createPassThroughWallet(params_);
     }
+
+    function testFuzz_createPassThroughWallet_createsClone_code(PassThroughWalletImpl.InitParams calldata params_)
+        public
+    {
+        clone = address(passThroughWalletFactory.createPassThroughWallet(params_));
+
+        test_clone_code();
+    }
+
+    function testFuzz_createPassThroughWallet_createsClone_canReceiveETH(PassThroughWalletImpl.InitParams calldata params_, uint96 amount_)
+        public
+    {
+        clone = address(passThroughWalletFactory.createPassThroughWallet(params_));
+        amount = amount_;
+
+        test_clone_canReceiveETH();
+    }
+
+    function testFuzz_createPassThroughWallet_createsClone_canDelegateCall(PassThroughWalletImpl.InitParams calldata params_, bytes calldata data_)
+        public
+    {
+        vm.assume(data_.length > 0);
+
+        clone = address(passThroughWalletFactory.createPassThroughWallet(params_));
+        data = data_;
+
+        test_clone_canDelegateCall();
+    }
+
 }
